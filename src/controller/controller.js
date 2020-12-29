2d797ca83737e9a85ab8ce2f63df8310ccde09c3
monaliza-vitor <vitormonaliza@gmail.com>
2020-12-29 18:30:34 -0300

    update ready

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -11,8 +11,8 @@ const createProduct = (req, res) => {
             res.status(500).send(error)
     })
 
-    res.status(201).send({ 
-        message: 'Produto criado com sucesso !', newProduct 
+    res.status(201).send({
+        message: 'Produto criado com sucesso !', newProduct
     })
 
 }
@@ -22,11 +22,70 @@ const getProducts = (req, res) => {
     productCollection.find((error, product) => {
         if (error)
             return res.status(500).send(error)
+
         return res.status(200).send(product)
     })
 }
 
+
+const getById = (req, res) => {
+
+    const id = req.params.id
+
+    productCollection.findById(id, (error, product) => {
+        if (error)
+            return res.status(404).send({ message: 'Id do produto não foi encontrado!' })
+
+        return res.status(200).send({ message: 'Produto encontrado!', product })
+
+    })
+
+}
+
+const getByCategory = (req, res) => {
+
+    const category = req.params
+
+    productCollection.find(category, (error, product) => {
+        if (error)
+            return res.status(500).send(error)
+
+        if (product)
+            return res.status(200).send({ message: 'Category found successfully! ', product })
+
+        return res.status(404).send({ message: 'Category is not found!' })
+
+    })
+
+}
+
+const updateProduct = (req, res) => {
+
+    const id = req.params.id
+    const body = req.body
+    const update = { new: true }
+
+    productCollection.findByIdAndUpdate(
+        id,
+        body,
+        update,
+        (error, product) => {
+            if (error)
+                return res.status(500).send(error)
+
+            return res.status(200).send({
+                message: 'Successfully edited product! ', product
+            })
+
+        }
+    )
+
+}
+
 module.exports = {
-    createProduct, 
-    getProducts
+    createProduct,
+    getProducts,
+    getById,
+    getByCategory,
+    updateProduct
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,9 +3,21 @@ const router = express.Router()
 const controller = require('../controller/controller')
 
 
-//cadastro
+//PRODUCT REGISTRATION
 router.post('/create', controller.createProduct)
-    
+
+//GET ALL PRODUCTS
 router.get('/products', controller.getProducts)
+
+//GET PRODUCT BY ID
+router.get('/product/:id', controller.getById)
+
+//GET PRODUCTS BY CATEGORY
+router.get('/products/:category', controller.getByCategory)
+
+//UPDATE PRODUCTS BY BODY
+router.put('/product/update/:id', controller.updateProduct)
+
+
 
 module.exports = router
